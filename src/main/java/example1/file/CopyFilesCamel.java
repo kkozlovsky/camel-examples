3baa8af387ee86f0c34c34d0b9298c1e5c2fff0d
kkozlovsky <kkozlovsky@yandex.ru>
2018-05-13 22:50:50 +0300

    Hello initial commit

---
+++ src/main/java/example1/file/CopyFilesCamel.java
@@ -0,0 +1,29 @@
+package example1.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFilesCamel {
+
+	public static void main(String[] args) {
+
+		CamelContext camelContext = new DefaultCamelContext();
+		try {
+			camelContext.addRoutes(new RouteBuilder() {
+				@Override
+				public void configure() throws Exception {
+					from("file:data/input?noop=true")
+					.to("file:data/output"); // будет создана, если не существует
+				}
+			});
+			
+			camelContext.start();
+			Thread.sleep(5000);
+			camelContext.stop();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
